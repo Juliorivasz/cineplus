736d7a42efb61865f26431b2cfd5030a1309ce5d
rivas.julio <juliorivasdev@gmail.com>
2023-12-10 09:45:10 -0300

    build and design login in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import { Login } from './admin/login';
+import Home from './components/Home';
+import Premieres from './components/Premieres';
+import { Movies } from './components/Movies';
+import { Series } from './components/Series';
+import { Anime } from './components/Anime';
+import { Movie } from './components/Movie';
 
 
 function App() {
@@ -9,7 +17,18 @@ function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/admin" element={<Login/>}></Route>
+        <Route path="/" element={<Main/>}>
+          <Route index element={<Home toggleNavigate={()=>{}} />} />
+          <Route path="estrenos" element={<Premieres />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="series" element={<Series />} />
+          <Route path="anime" element={<Anime />} />
+          <Route path="movie" element={<Movie />} />
+          <Route path="contactos" element={<Premieres />} />
+        </Route>
+      </Routes>
       <Footer />
     </>
   )
